869066d18c6f47a77acbebb496cc8c6dc1282675
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2023-11-22 18:13:57 +0100

    EMS and coomonjs

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 module.exports = {
     database: {
       host: 'localhost',
-      user: 'dspa',
-      password: 'your_password',
+      user: 'postgres',
+      password: 'dspa',
       database: 'dspa',
+      port: 5432
     },
     server: {
       port: 3000,
     },
+    frontend: {
+      path: '../dspa-frontend/'
+    }
   };
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import express from "express";
-import morgan from "morgan";
-import bodyParser from "body-parser";
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+//const morgan = require('morgan');
+//const pg = require('pg');
+//const bodyParser = require('body-parser');
+//const express = require('express');
 
+import * as config from './config/config.js'
 
-
-
-const express = require('express');
-const config = require('./config/config');
+//const config = require('./config/config');
 const router = express.Router();
 
+// get database
+const db = pg.Client({
+  user: config.database.user,
+  host: config.database.host,
+  database: config.database.database,
+  password: config.database.password,
+  port: config.database.port,
+})
+
+// connect to databse
+db.connect();
 
 // const cors = require('cors');
 
@@ -27,19 +41,37 @@ app.use(morgan("combined"))
 // Use API routes
 //app.use('/api', apiRoutes);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
 });
 
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
 
-module.exports = router;
-
-// Start the server
-app.listen(config.server.port, () => {
-  console.log(`Server is running on port ${config.server.port}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
