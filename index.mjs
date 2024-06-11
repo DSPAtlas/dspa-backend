55c49a8ef417e01610574e7fb5a71db6512cb994
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2024-06-11 21:47:31 +0200

    refactor

--- index.mjs
+++ index.mjs
@@ -1,19 +1,4 @@
-// const express = require('express');
 
-// const apiRoutes = require('./routes/api');
-// const config = require('./config/config');
-// const connection = require('./db/connection');
-
-// // Handle graceful shutdown
-// process.on('SIGINT', () => {
-//   connection.end((err) => {
-//     if (err) {
-//       console.error('Error closing MySQL connection:', err);
-//     } else {
-//       console.log('MySQL connection closed');
-//     }
-//     process.exit();
-//   });
 import config from './config/config.mjs';
 import express from 'express';
 import routes from './routes/index.mjs';
