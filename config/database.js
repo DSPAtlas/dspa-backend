921ed79ddbffc09530f3e5de4ab27558b6928e3a
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2025-03-13 17:14:17 +0100

    fix configs

--- config/database.js
+++ config/database.js
@@ -2,15 +2,15 @@
 import mysql from 'mysql2';
 
 const db = mysql.createPool({
-  host: "localhost", //'mysql',
-  user: 'root',  
-  //password: process.env.DB_PASSWORD || "dspa",
-  database: process.env.DB_NAME || "dynaprotdb",
-  port:  3306,
-  //waitForConnections: true,
-  //connectionLimit: 10,
- // queueLimit: 0,
-  //connectTimeout: 10000 // 10 seconds
+  host: process.env.HOST,
+  user: process.env.USER,  
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000 // 10 seconds
 }).promise();
 
 export default db;
