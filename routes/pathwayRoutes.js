fc0b1d3804c68cbf1eb0b45f089c6a09f931cb34
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2025-05-22 15:24:31 +0200

    Delete routes/pathwayRoutes.js

--- routes/pathwayRoutes.js
+++
@@ -1,7 +0,0 @@
-// import express from 'express';
-// import { getKeggPathwayData } from '../controllers/pathwayController.js';
-
-// const router = express.Router();
-
-// router.get('/', getKeggPathwayData);
-// export default router;
